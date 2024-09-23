01d9bd0475b4a1a4640bba8662ce164b14ec4f18
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-09-23 18:50:07 -0400

    feat: update GET functionality to request all items from pg database

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,3 +1,11 @@
-exports.getItems = (req, res) => {
-  res.json({ message: "Returning all items" });
+const pool = require("../db");
+
+exports.getItems = async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM items");
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error fetching items: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
